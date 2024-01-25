4407a88ce7f335ff00ff5c07f81791961477ea82
alfredothepasta <robertalexreed@gmail.com>
2024-01-25 10:04:04 -0700

    Added comments to HomeController.cs Updated the check for user logic

--- LMSEarlyBird/Controllers/HomeController.cs
+++ LMSEarlyBird/Controllers/HomeController.cs
@@ -5,44 +5,67 @@ using Microsoft.AspNetCore.Identity;
 using LMSEarlyBird.Data;
 using System.Numerics;
 
+
 namespace LMSEarlyBird.Controllers
 {
+    /// <summary>
+    /// Controller that handles the home pages
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// context variable for accessing the db
+        /// </summary>
         private readonly ApplicationDbContext _context;
+        /// <summary>
+        /// Context accessor for reading session data
+        /// </summary>
         private readonly IHttpContextAccessor _contextAccessor;
 
+        /// <summary>
+        /// Constructor, initializes the instance variables
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="contextAccessor"></param>
         public HomeController(ApplicationDbContext context, IHttpContextAccessor contextAccessor)
         {
             _context = context;
             _contextAccessor=contextAccessor;
         }
 
+        /// <summary>
+        /// Handles the logic for the Index page
+        /// </summary>
+        /// <returns></returns>
         public async Task<IActionResult> Index()
         {
-            try
+            // Checks to see if there is a current signed in user
+            if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
             {
                 var currentUser = _contextAccessor.HttpContext?.User.GetUserId();
-                if(currentUser != null)
+                if (currentUser != null)
                 {
                     AppUser userData = await _context.Users.FindAsync(currentUser);
                     return View(userData);
                 }
-            } catch
-            {
-            
             }
 
             return View();
-
-
         }
 
+        /// <summary>
+        /// Returns the Privacy page
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Privacy()
         {
             return View();
         }
 
+        /// <summary>
+        /// Returns the error page
+        /// </summary>
+        /// <returns></returns>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {

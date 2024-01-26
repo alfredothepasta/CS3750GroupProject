7a4323dfbc976b601cbac6fed9108552410bd86e
davidrobertsweber <davidrobert@mail.weber.edu>
2024-01-26 11:58:59 -0700

    Working on calendar

---
+++ LMSEarlyBird/wwwroot/js/Calendar.js
@@ -0,0 +1,12 @@
+﻿let calendarEl = document.getElementById('calendar');
+
+let calendar = new FullCalendar.Calendar(calendarEl, {
+    initalView: 'dayGridMonth',
+    headertoolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+    },
+    selectable: true
+});
+calendar.render();

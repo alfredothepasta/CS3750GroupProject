d5153e9037495e78e4ea88152855474a6b0b5b23
alfredothepasta <45605623+alfredothepasta@users.noreply.github.com>
2024-01-30 10:19:40 -0700

    commented some nonsense out

--- LMSEarlyBird/Program.cs
+++ LMSEarlyBird/Program.cs
@@ -25,11 +25,11 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 var app = builder.Build();
 
-if (args.Length == 1 && args[0].ToLower() == "seeddata")
-{
-    await Seed.SeedUsersAndRolesAsync(app);
-    //Seed.SeedData(app);
-}
+//if (args.Length == 1 && args[0].ToLower() == "seeddata")
+//{
+//    await Seed.SeedUsersAndRolesAsync(app);
+//    //Seed.SeedData(app);
+//}
 
 
 // Configure the HTTP request pipeline.

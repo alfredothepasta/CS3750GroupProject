b8bbc12bc1be49688102acd600cf2ad46bca1d26
Ziggy3060 <108693265+Ziggy3060@users.noreply.github.com>
2024-02-10 15:49:07 -0700

    Dashboard now has links for future use to go to the courses.  It also now displays all of the information for each course. Assignment with due dates list is still static.

--- LMSEarlyBird/Controllers/UserController.cs
+++ LMSEarlyBird/Controllers/UserController.cs
@@ -11,50 +11,55 @@ namespace LMSEarlyBird.Controllers
     public class UserController : Controller
     {
         /// <summary>
-        /// context variable for accessing the db
-        /// </summary>
-        private readonly ApplicationDbContext _context;
-        /// <summary>
-        /// Context accessor for reading session data
+        /// Context accessor for reading Session data
         /// </summary>
         private readonly IHttpContextAccessor _contextAccessor;
-        private readonly IUserIdentityService _userIdentityService;
-        private readonly IAppUserRepository _appUserRepository;
-        private readonly ICourseRepository _courseRepository;
-        private readonly IStudentCourseRepository _studentCourseRepository;
-        private readonly IDepartmentRepository _departmentRepository;
         /// <summary>
-        /// Context for accessing the address database
+        /// Context accessor for reading User Identification data
         /// </summary>
-        private readonly IAddressRepository _addressRepository;
+        private readonly IUserIdentityService _userIdentityService;
+        /// <summary>
+        /// Context accessor for reading User data
+        /// </summary>
+        private readonly IAppUserRepository _appUserRepository;
+        /// <summary>
+        /// Context accessor for reading Courses database
+        /// </summary>
+        private readonly ICourseRepository _courseRepository;
+        /// <summary>
+        /// Context accessor for reading Departments database
+        /// </summary>
+        private readonly IDepartmentRepository _departmentRepository;
         /// <summary>
         /// Context for accessing the room database
         /// </summary>
         private readonly IRoomRepository _roomRepository;
+        /// <summary>
+        /// Context for accessing the building database
+        /// </summary>
+        private readonly IBuildingRepository _buildingRepository;
 
         /// <summary>
         /// Constructor, initializes the instance variables
         /// </summary>
         /// <param name="context"></param>
         /// <param name="contextAccessor"></param>
-        public UserController(ApplicationDbContext context, 
+        public UserController(
             IHttpContextAccessor contextAccessor, 
             ICourseRepository courseRepository, 
             IUserIdentityService userIdentityService,
             IAppUserRepository appUserRepository,
-            IStudentCourseRepository studentCourseRepository, 
             IDepartmentRepository departmentRepository,
-            IAddressRepository addressRepository,
-            IRoomRepository roomRepository)
+            IRoomRepository roomRepository,
+            IBuildingRepository buildingRepository)
         {
-            _context = context;
             _contextAccessor = contextAccessor;
             _courseRepository = courseRepository;
             _userIdentityService = userIdentityService;
             _appUserRepository = appUserRepository;
-            _studentCourseRepository = studentCourseRepository;
             _departmentRepository = departmentRepository;
             _roomRepository = roomRepository;
+            _buildingRepository = buildingRepository;
         }
 
         private string FormatDaysOfWeek(string daysOfWeek)
@@ -73,7 +78,7 @@ namespace LMSEarlyBird.Controllers
         /// Returns the Dashboard page with login information if correct login, else returns to login with errors
         /// </summary>
         /// <returns></returns>
-        public async Task<IActionResult> /*Task<List<RegisterCourseViewModel>>*/ Dashboard(string? BuildingName = "")
+        public async Task<IActionResult> Dashboard(string? BuildingName = "")
         {
             // Checks to see if there is a current signed in user
             if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated)
@@ -81,37 +86,27 @@ namespace LMSEarlyBird.Controllers
                 var currentUser = _contextAccessor.HttpContext?.User.GetUserId();
                 if (currentUser != null)
                 {
-                    //AppUser userData = await _context.Users.FindAsync(currentUser);
-                    //return View(userData);
-
                     //pull user based on logged in user
                     string userId = _userIdentityService.GetUserId();
                     AppUser profile = await _appUserRepository.GetUser(userId);
 
-                    //pull the current courses
+                    //pull the current courses, required for obtaining department names
                     var courses = await _courseRepository.GetAllCoursesWithInstructor();
-
-                    //Address userAddress = new Address();
-                    //userAddress = await _addressRepository.getUserAddress(userId);
-
-
-
                     //Create list of department names
                     RegistrationViewModel result = new RegistrationViewModel();
                     List<string> departmentNames = _departmentRepository.GetAllDepartments().Result.Select(x => x.DeptName).ToList();
                     result.DepartmentNames = departmentNames;
 
-                    // Create list of room numbers & Buildings
+                    // Create list of room numbers
                     AddCourseViewModel roomInfo = new AddCourseViewModel();
                     List<Room> roomNumber = _roomRepository.GetRooms().Result.Select(x => new Room { RoomNumber = x.RoomNumber }).ToList();
-                    List<Room> building = _roomRepository.GetRooms().Result.Select(x => new Room { BuildingID = x.BuildingID }).ToList();
-                    string buildingName = _roomRepository.GetRoomById(building[0].BuildingID).Result.ToString();
-                    BuildingName = buildingName;
                     roomInfo.RoomList = roomNumber;
 
-                    //List<StudentCourse> SCourses = new List<StudentCourse>();
+                    // Create list of Buildings
+                    List<Building> buildings = _buildingRepository.GetBuildings().Result.ToList();
+                    roomInfo.BuildingList = buildings;
 
-                    // build the model view
+                    // build the model view for the user to display First and Last name as well as the courses//
                     var userVM = new AppUser
                     {
                         FirstName = profile.FirstName,
@@ -121,32 +116,8 @@ namespace LMSEarlyBird.Controllers
                         
                     };
                     return View(userVM);
-                    //List<RegisterCourseViewModel> result = new List<RegisterCourseViewModel>();
-                    //foreach (var course in courses)
-                    //{
-                    //    var registrationViewModel = new RegisterCourseViewModel
-                    //    {
-                    //        Id = course.id,
-                    //        CourseName = course.CourseName,
-                    //        CourseNumber = course.CourseNumber,
-                    //        CreditHours = course.CreditHours,
-                    //        StartTime = course.StartTime,
-                    //        EndTime = course.EndTime,
-                    //        IsRegistered = profile.StudentCourses.Any(x => x.CourseId == course.id),
-                    //        Department = course.Department.DeptCode,
-                    //        InstructorName = course.InstructorCourses.FirstOrDefault().User.FirstName + " " + course.InstructorCourses.FirstOrDefault().User.LastName,
-                    //        DaysOfWeek = FormatDaysOfWeek(course.DaysOfWeek),
-                    //    };
-
-                    //    bool temp = profile.StudentCourses.Any(x => x.CourseId == course.id);
-
-                    //    result.Add(registrationViewModel);
-                    //}
-                    //return result;
                 }
             }
-
-            //return RedirectToActionResult("Account", "Login");
             return View("Account", "Login");
         }
 

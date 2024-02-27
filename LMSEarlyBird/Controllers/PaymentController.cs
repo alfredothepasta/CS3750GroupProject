7dfd40cec98f646d36b7647cd442aa30164c663f
Ziggy3060 <108693265+Ziggy3060@users.noreply.github.com>
2024-02-26 18:47:54 -0700

    Had some ideas for cleaning things up, also tried some suggestions based on Stripes website.  Currently with errors, saving to backup.

--- LMSEarlyBird/Controllers/PaymentController.cs
+++ LMSEarlyBird/Controllers/PaymentController.cs
@@ -4,6 +4,10 @@ using LMSEarlyBird.Repository;
 using LMSEarlyBird.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Stripe.Checkout;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Security.Policy;
+using System.Text.Encodings.Web;
+using Stripe;
 
 namespace LMSEarlyBird.Controllers
 {
@@ -60,12 +64,51 @@ namespace LMSEarlyBird.Controllers
         [HttpGet]
         public async Task<IActionResult> Checkout(PaymentViewModel paymentVM)
         {
+            string userId = _userIdentityService.GetUserId();
+
+            PaymentViewModel paymentVM2 = new PaymentViewModel
+            {
+                PaymentAmount = paymentVM.PaymentAmount
+            };
+
+            // create payment intent
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = Convert.ToInt64(paymentVM.PaymentAmount) * 100,
+                Currency = "usd",
+                AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions
+                {
+                    Enabled = true,
+                },
+            };
+            var service = new PaymentIntentService();
+            service.Create(options);
+
             // pass on the payment view model for the payment amount
-            return View(paymentVM);
+            return View(paymentVM2);
         }
         [HttpGet]
-        public async Task<IActionResult> Cancel()
+        public async Task<IActionResult> Cancel(PaymentViewModel paymentVM)
         {
+            //StripeConfiguration.ApiKey = "sk_test_51Ogs9OA3uLdsTHsjYGPdb0TTncfQoAirAiKbDqVRhHD13TwX28i76mFpSLtHUXssoXoATUlQDGXTScu1e27UJsWp00TfVoL8CR";
+            var service = new PaymentIntentService();
+            service.Get("pi_3MtwBwLkdIwHu7ix28a3tqPa");
+            //var service = new ChargeService();
+            //service.Get("ch_3MmlLrLkdIwHu7ix0snN0B15");
+
+            //(async () => {
+            //    const { paymentIntent, error} = await stripe.confirmCardPayment(clientSecret);
+            //    if (error)
+            //    {
+            //        // Handle error here
+            //    }
+            //    else if (paymentIntent && paymentIntent.status == "succeeded")
+            //    {
+            //        // Handle successful payment here
+            //    }
+            //})();
+
+
             return View();
         }
         [HttpPost]
@@ -122,47 +165,56 @@ namespace LMSEarlyBird.Controllers
         },
                 Mode = "payment",
                 SuccessUrl = "https://localhost:7243/Payment/Success",
+                //CancelUrl = "https://localhost:7243/Payment/Cancel?paymentVM={paymentVM}",
                 CancelUrl = "https://localhost:7243/Payment/Cancel",
             };
 
             var service = new SessionService();
             Session session = service.Create(options);
 
-            Response.Headers.Add("Location", session.Url);
-            return new StatusCodeResult(303);
-        }
-
-        [HttpPost("create-checkout-session")]
-        public ActionResult CreateCheckoutSession()
-        {
-            var options = new SessionCreateOptions
+            // update payment intent
+            var options2 = new PaymentIntentUpdateOptions
             {
-                LineItems = new List<SessionLineItemOptions>
-        {
-          new SessionLineItemOptions
-          {
-            PriceData = new SessionLineItemPriceDataOptions
-            {
-              UnitAmount = 2000,
-              Currency = "usd",
-              ProductData = new SessionLineItemPriceDataProductDataOptions
-              {
-                Name = "T-shirt",
-              },
-            },
-            Quantity = 1,
-          },
-        },
-                Mode = "payment",
-                SuccessUrl = "https://localhost:7243/Payment/success",
-                CancelUrl = "https://localhost:7243/Payment/cancel",
+                Metadata = new Dictionary<string, string> { { "order_id", "6735" } },
             };
-
-            var service = new SessionService();
-            Session session = service.Create(options);
+            var service2 = new PaymentIntentService();
+            service2.Update("pi_3MtwBwLkdIwHu7ix28a3tqPa", options2);
 
             Response.Headers.Add("Location", session.Url);
             return new StatusCodeResult(303);
         }
+
+        //[HttpPost("create-checkout-session")]
+        //public ActionResult CreateCheckoutSession()
+        //{
+        //    var options = new SessionCreateOptions
+        //    {
+        //        LineItems = new List<SessionLineItemOptions>
+        //{
+        //  new SessionLineItemOptions
+        //  {
+        //    PriceData = new SessionLineItemPriceDataOptions
+        //    {
+        //      UnitAmount = 2000,
+        //      Currency = "usd",
+        //      ProductData = new SessionLineItemPriceDataProductDataOptions
+        //      {
+        //        Name = "T-shirt",
+        //      },
+        //    },
+        //    Quantity = 1,
+        //  },
+        //},
+        //        Mode = "payment",
+        //        SuccessUrl = "https://localhost:7243/Payment/success",
+        //        CancelUrl = "https://localhost:7243/Payment/cancel",
+        //    };
+
+        //    var service = new SessionService();
+        //    Session session = service.Create(options);
+
+        //    Response.Headers.Add("Location", session.Url);
+        //    return new StatusCodeResult(303);
+        //}
     }
 }

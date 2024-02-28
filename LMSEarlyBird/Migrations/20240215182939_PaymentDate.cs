6ee5e541d73e7e5ccbea9a074d268f8deeaddc66
alfredothepasta <45605623+alfredothepasta@users.noreply.github.com>
2024-02-27 17:30:37 -0700

    Added migrations back to version control

---
+++ LMSEarlyBird/Migrations/20240215182939_PaymentDate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMSEarlyBird.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PaymentTime",
+                table: "PaymentHistory",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentTime",
+                table: "PaymentHistory");
+        }
+    }
+}
